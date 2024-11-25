a6069f2e650dd224994d9a598c49f7e13a3501b9
Jubilee Marshall <jam1703@nyu.edu>
2024-11-24 19:24:19 -0500

    readme

--- HW11.ino
+++ HW11.ino
@@ -1,34 +1,34 @@
-int ledPins[] = {10, 9, 8, 4, 3, 2}; // reversed the order to see why my original code wasn't working 
+int ledPins[] = {10, 9, 8, 4, 3, 2};  // LED pins in reverse order
 int currentLed = 0;  
 int previousMillis = 0;
 int start = 0;  
-int interval = 10000; //shorten to test code 
-int oneMinute = 60000;  
+int interval = 10000;  // Time interval to light up the next LED
+intg oneMinute = 60000;  // Time duration to keep LEDs on before turning off
 
 void setup() {
   for (int i = 0; i < 6; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
-  start = millis();  
+  start = millis();  // Record the start time
+  digitalWrite(ledPins[0], HIGH);  // Immediately light up the first LED
+  currentLed = 1;  // Set currentLed to 1 because the first LED is already on
 }
 
 void loop() {
-  int now = millis();  
+int now = millis();  
+  
+  // After 60 seconds, turn off all LEDs
   if (now - start >= oneMinute) {
-    // After 60 seconds, turn off all LEDs
     for (int i = 0; i < 6; i++) {
-      digitalWrite(ledPins[i], LOW);  
+      digitalWrite(ledPins[i], LOW);  // Turn off all LEDs
     }
-  return; 
+    return;  // Exit the loop
   }
 
   // Move to the next LED every 10 seconds
   if (now - previousMillis >= interval && currentLed < 6) {
     previousMillis = now;  // Reset the timer for the next LED
-    currentLed++;  // Move to the next LED in the line
-  }
-
-  for (int i = 0; i < currentLed; i++) {
-    digitalWrite(ledPins[i], HIGH);  // turns on the current LED 
+    digitalWrite(ledPins[currentLed], HIGH);  // Turn on the next LED
+    currentLed++;  // Move to the next LED
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HW11
  
+ This one was a pain! I wanted to use my 6 LEDs so that each would sequentially fade in over the course of 10 seconds until all 6 were lit. The code itself was okay but then the last LED wouldn't light up. I troubleshooted by making sure the wiring was correct and the LED would light up using digitalWrite and no extraneous code, changing to pin 10 in case 12 didn't support analogWrite, reversing the order of the PINS in the sequence, and eventually asking ChatGPT which instructed me to use the serial monitor to make sure that power was going to all the pins correctly (it was). I still don't know what wasn't working! In the end I kept changing things slightly and getting frustrated so I just used digitalWrite and called it a day. If I were to go back in I would have the first LED light up immediately instead of at the end of 10 seconds and also use modulus to have the whole thing cycle. 
+
+![Circuit diagram](./circuit.png)
+
+![Video](./LED clock.mp4)
